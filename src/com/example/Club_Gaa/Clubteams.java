319dc441df984ef4690b43007d75dd7b83a74319
liamkeane93 <g00287233@gmit.ie>
2016-03-25 18:34:19 +0000

    Server/Database Update

---
+++ src/com/example/Club_Gaa/Clubteams.java
@@ -0,0 +1,49 @@
+package com.example.Club_Gaa;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Clubteams extends TeamHome {
+    Button Intermediate;
+    Button Senior;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clubcategory);
+
+        //final Context context = this;
+        Intermediate = (Button) findViewById(R.id.Intermediate);
+        Intermediate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Clubteams.this, IntermediateTeams.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+        Senior = (Button) findViewById(R.id.Senior);
+        Senior.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Clubteams.this, SeniorTeams.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+}
