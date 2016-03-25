4e8163a6673aa3a879a8ff98fcafa34acf40c10a
liamkeane93 <g00287233@gmit.ie>
2016-03-25 18:37:11 +0000

    Server/Database Update

---
+++ src/com/example/Club_Gaa/TeamHome.java
@@ -0,0 +1,67 @@
+package com.example.Club_Gaa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Liam on 25/02/2016.
+ */
+public class TeamHome extends MyActivity {
+    Button ClubTeams;
+    Button CountyTeams;
+    Button PersonalTeams;
+
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teamhomepage);
+
+        //final Context context = this;
+        ClubTeams = (Button) findViewById(R.id.ClubTeams);
+        ClubTeams.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TeamHome.this,Clubteams.class);
+
+                startActivity(intent);
+
+
+
+            }
+
+        });
+
+        CountyTeams = (Button) findViewById(R.id.CountyTeams);
+        CountyTeams.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TeamHome.this, Countyteams.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+        PersonalTeams = (Button) findViewById(R.id.PersonalTeams);
+        PersonalTeams.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TeamHome.this, Database.class);
+
+                startActivity(intent);
+            }
+
+        });
+    }
+}
+
