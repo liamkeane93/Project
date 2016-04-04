eac1b8861fc4a13699febe442bc055bc25458eb8
liamkeane93 <g00287233@gmit.ie>
2016-04-04 17:15:59 +0100

    timer added

--- src/com/example/Club_Gaa/ScoreBoard.java
+++ src/com/example/Club_Gaa/ScoreBoard.java
@@ -1,7 +1,10 @@
 package com.example.Club_Gaa;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -13,11 +16,91 @@ public class ScoreBoard extends MyActivity {
     int TeamBPoint = 0;
     int TeamBGoal =0;
 
+
+    private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
+
+    private TextView timerValue;
+
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.score);
+        timerValue = (TextView) findViewById(R.id.timerValue);
+
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+
+            }
+        });
+
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+
+            }
+        });
+        resetButton = (Button) findViewById(R.id.resetButton);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                timeInMilliseconds = 0L;
+                timeSwapBuff = 0L;
+                updatedTime = 0L;
+
+                int secs = (int) (updatedTime / 1000);
+                int mins = secs / 60;
+                secs = secs % 60;
+                int milliseconds = (int) (updatedTime % 1000);
+                timerValue.setText("" + mins + ":"
+                        + String.format("%02d", secs) + ":"
+                        + String.format("%03d", milliseconds));
+
+
+            }
+        });
+
     }
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+
+    };
+
 
     public void teamAPoint(View view) {
 
@@ -61,7 +144,7 @@ public class ScoreBoard extends MyActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void resetButton(View view) {
+    public void resetButton2(View view) {
 
         TeamAPoint = 0;
         TeamAGoal =0;
@@ -73,7 +156,7 @@ public class ScoreBoard extends MyActivity {
       //  displayForTeamB(0);
 
     }
-    public void resetButton2(View view) {
+    public void resetButton(View view) {
 
        // TeamAPoint = 0;
        // TeamAGoal =0;
@@ -85,4 +168,5 @@ public class ScoreBoard extends MyActivity {
         displayForTeamB(0);
 
     }
+
 }
